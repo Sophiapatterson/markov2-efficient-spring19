63c21ce24c3b5d23fba16718457d963c6ec2630f
Sophia Patterson <Sophia.patterson@duke.edu>
2019-03-01 23:21:45 -0500

    sop

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -3,28 +3,25 @@ import java.util.*;
 public class EfficientMarkov extends BaseMarkov {
 	private Map<String,ArrayList<String>> myMap;
 	
-	public EfficientMarkov(){
-		this(3);
-	}
-	
 	public EfficientMarkov(int order) {
 		super(order);
 		myMap = new HashMap<>();
+	}
+	public EfficientMarkov(){
+		this(3);
 	}
 	@Override
 	public void setTraining(String text) {
 		myText = text;
 		myMap.clear();
-		for (int i=0; i<=(myText.length() - myOrder); i++) {
-			myMap.putIfAbsent(text.substring(i,i+myOrder), new ArrayList<String>());
-			if (text.substring(i+myOrder)!= null){
-				myMap.get(text.substring(i+myOrder)).add(text.substring(i+myOrder+1));
+		for (int i=0; i<=myText.length() - myOrder; i++) {
+			myMap.putIfAbsent(myText.substring(i,i+myOrder), new ArrayList<String>());
+			if(i==myText.length() - myOrder) {
+				myMap.get(myText.substring(i+myOrder)).add(PSEUDO_EOS);
 			}
 			else {
-				myMap.get(text.substring(i+myOrder)).add(PSEUDO_EOS);
-			}
-			
-			
+				myMap.get(myText.substring(i+myOrder)).add(myText.substring(i+myOrder, i+myOrder+1));
+			}		
 		}
 	}
 	@Override
