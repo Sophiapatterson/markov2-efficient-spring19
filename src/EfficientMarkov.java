9da653c17c0a23d0b8b54f40026ed2a9086c6fc0
Sophia Patterson <Sophia.patterson@duke.edu>
2019-03-01 22:19:38 -0500

    so9p

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -3,7 +3,35 @@ import java.util.*;
 public class EfficientMarkov extends BaseMarkov {
 	private Map<String,ArrayList<String>> myMap;
 	
-	EfficientMarkov(){
+	public EfficientMarkov(){
+		this(3);
+	}
+	
+	public EfficientMarkov(int order) {
+		super(order);
 		myMap = new HashMap<>();
+	}
+	@Override
+	public void setTraining(String text) {
+		myText = text;
+		myMap.clear();
+		for (int i=0; i<=(myText.length() - myOrder); i++) {
+			myMap.putIfAbsent(text.substring(i,i+myOrder), new ArrayList<String>());
+			if (text.substring(i+myOrder)!= null){
+				myMap.get(text.substring(i+myOrder)).add(text.substring(i+myOrder+1));
+			}
+			else {
+				myMap.get(text.substring(i+myOrder)).add(PSEUDO_EOS);
+			}
+			
+			
+		}
+	}
+	@Override
+	public ArrayList<String> getFollows(String key) {
+		if (! myMap.containsKey(key)) {
+			throw new NoSuchElementException(key+" not in map");
+		}
+		return myMap.get(key);
 	}
 }	
---
+++ src/EfficientWordMarkov.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class EfficientWordMarkov extends BaseWordMarkov{
+	private Map<WordGram,ArrayList<String>> myMap;
+
+	public EfficientWordMarkov(int order) {
+		super(order);
+		myMap = new HashMap<>();
+			
+		}
+	public EfficientWordMarkov() {
+		this(2);
+	}
+	@Override
+	public void setTraining(String text) {
+		myWords = text.split("\\s+");
+		for (int i=0; i<myWords.length-myOrder; i++) {
+			WordGram var = new WordGram(myWords, i, myOrder);
+			myMap.putIfAbsent(var, new ArrayList<String>());
+			if(i==myWords.length-myOrder) {
+				myMap.get(var).add(PSEUDO_EOS);
+			}
+			else {
+				myMap.get(var).add(myWords[i+myOrder]);
+			}
+		}
+	}
+	@Override
+	public ArrayList<String> getFollows(WordGram key){
+		if (! myMap.containsKey(key)) {
+			throw new NoSuchElementException(key+" not in map");
+		}
+		else {
+			return myMap.get(key);
+		}
+	}
+}
